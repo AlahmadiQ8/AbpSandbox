652dc4f0c239d2f8127eb307b087824dad0653c0
Mohammad Mohammad <mmohammadq8@outlook.com>
2022-05-30 23:58:37 +0300

    added create book

---
+++ Sanbox.Main/Pages/BookStorePageModel.cs
@@ -0,0 +1,12 @@
+﻿using Sanbox.Main.Localization;
+using Volo.Abp.AspNetCore.Mvc.UI.RazorPages;
+
+namespace Sanbox.Main.Pages;
+
+public abstract class BookStorePageModel : AbpPageModel
+{
+    protected BookStorePageModel()
+    {
+        LocalizationResourceType = typeof(MainResource);
+    }
+}
---
+++ Sanbox.Main/Pages/Books/CreateModal.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Sanbox.Main.Services;
+using Sanbox.Main.Services.Dtos;
+
+namespace Sanbox.Main.Pages.Books;
+
+public class CreateModalModel : BookStorePageModel
+{
+    [BindProperty]
+    public CreateUpdateBookDto Book { get; set; }
+    
+    private readonly IBookAppService _bookAppService;
+
+    public CreateModalModel(IBookAppService bookAppService)
+    {
+        _bookAppService = bookAppService;
+    }
+    
+    public void OnGet()
+    {
+        Book = new CreateUpdateBookDto();
+    }
+    
+    public async Task<IActionResult> OnPostAsync()
+    {
+        await _bookAppService.CreateAsync(Book);
+        return NoContent();
+    }
+}
